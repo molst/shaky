251763e390a1fa4fdd7ea6c9e8a392d894be637d
molst <molst@yahoo.com>
2013-12-05 22:16:37 +0100

    added url

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject shaky "0.1-SNAPSHOT"
+(defproject shaky "0.2-SNAPSHOT"
   :description "A web development helper library."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
+  :url "https://github.com/molst/shaky"
+  :scm {:name "git" :url "https://github.com/molst/shaky"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.1.7"]
                  [http-kit "2.1.12"]
-                 [torpo "0.1-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
-  :scm {:name "git" :url "https://github.com/molst/shaky"})
+                 [torpo "0.2-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
