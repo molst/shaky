a5020074fd67751f78daee99868ab84d05a3ab82
molst <molst@yahoo.com>
2013-12-05 20:48:55 +0100

    no snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shaky "0.1-SNAPSHOT"
+(defproject shaky "0.1"
   :description "A web development helper library."
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.1.7"]
                  [http-kit "2.1.12"]
-                 [torpo "0.1-SNAPSHOT"]]
+                 [torpo "0.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
