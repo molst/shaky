4a72a798c0527ca04fb60219c512de8a8535ba11
molst <molst@yahoo.com>
2014-12-09 23:08:09 +0100

    fix lein 2.5 warnings

--- project.clj
+++ project.clj
@@ -6,13 +6,13 @@
   :url "https://github.com/molst/shaky"
   :scm {:name "git" :url "https://github.com/molst/shaky"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [torpo "0.2-SNAPSHOT"]
+                 [net.cgrand/moustache "1.1.0" :exclusions [org.clojure/clojure ring/ring-core]]
+                 [torpo "0.2-SNAPSHOT" :exclusions [org.clojure/clojure]]
                  [pisto "0.2-SNAPSHOT"]
                  [ring "1.3.1"]
-                 [http-kit "2.1.16"]
-                 [net.cgrand/moustache "1.1.0"]]
+                 [http-kit "2.1.16"]]
   :source-paths ["src/clj"]
-  :plugins [[com.keminglabs/cljx "0.3.2"]]
+  :plugins [[com.keminglabs/cljx "0.5.0"]]
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/classes"
                    :rules :clj}
@@ -25,6 +25,6 @@
                   {:source-paths ["dev/cljx"]
                    :output-path "target/dev-classes"
                    :rules :cljs}]}
-  :hooks [cljx.hooks]
+  :prep-tasks [["cljx" "once"] "javac" "compile"]
   :profiles {:dev {:source-paths ["target/dev-classes"] ;;for repl to recognize the dev ns after cljx
                    }})
